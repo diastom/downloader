2c3e3591a69ea63053651e50e82ac25755e49245
Walter Rodriguez <luxorimartin@gmail.com>
2025-09-23 03:54:22 +0330

    Update core.py

--- dl_bot/bot/core.py
+++ dl_bot/bot/core.py
@@ -1,10 +1,10 @@
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 
-from dl_bot.config import settings
-from dl_bot.utils.db_session import AsyncSessionLocal
-from dl_bot.bot.handlers import admin, common, downloader, settings as user_settings, video
-from dl_bot.bot.middlewares import DbSessionMiddleware
+from config import settings
+from utils.db_session import AsyncSessionLocal
+from bot.handlers import admin, common, downloader, settings as user_settings, video
+from bot.middlewares import DbSessionMiddleware
 
 
 def setup_dispatcher() -> Dispatcher:
