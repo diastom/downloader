12c5b63bde4088c98cb6234ddcc99f8ce54ee9a9
Walter Rodriguez <luxorimartin@gmail.com>
2025-09-25 01:35:14 +0330

    Add files via upload

---
+++ bot/core.py
@@ -0,0 +1,30 @@
+from aiogram import Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+from utils.bot_instance import bot 
+from config import settings
+from utils.db_session import AsyncSessionLocal
+from bot.handlers import admin, common, downloader, settings as user_settings, video
+from bot.middlewares import DbSessionMiddleware
+
+
+def setup_dispatcher() -> Dispatcher:
+    """
+    Creates and configures the main Dispatcher instance, registering all routers.
+    """
+    # Using simple in-memory storage for FSM. For production, RedisStorage is better.
+    storage = MemoryStorage()
+    dp = Dispatcher(storage=storage)
+
+    # Register our custom middleware to provide a session to each handler
+    dp.update.middleware(DbSessionMiddleware(session_pool=AsyncSessionLocal))
+
+    # Include all the routers from the handlers package
+    dp.include_router(common.router)
+    dp.include_router(admin.router)
+    dp.include_router(user_settings.router)
+    dp.include_router(video.router)
+    dp.include_router(downloader.router) # This should be last as it has a broad regex
+
+    return dp
+
+
